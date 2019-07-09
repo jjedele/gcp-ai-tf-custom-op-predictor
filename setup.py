5cca320d3c37e291135e41abd8a6310fc5132d97
Jeffrey Jedele <jeffrey.jedele@gmail.com>
2019-07-09 15:08:54 +0200

    Remove stale script from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="custom-op-tf-predictor",
     version="0.1",
-    scripts=["predictor.py", "dependencies.py"],
+    scripts=["predictor.py"],
     install_requires=[
         "tf-sentencepiece==0.1.82"
     ],
